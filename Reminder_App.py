7715ee0ed8cb016d12f8ba4effe3c3bd8b32fee3
sonu <sk2901@gmail.com>
2022-02-04 22:03:53 +0530

    initial commit

---
+++ Reminder_App.py
@@ -0,0 +1,12 @@
+
+import time
+from plyer import notification
+
+
+while True :
+        notification.notify(
+                title = " plz drink water !! ",
+                message = " drink water sonu which is required for your good health",
+                timeout = 2
+        )
+        time.sleep(6)   # ( secound * which is your time ketne deer bad bjna he wo ayega yha per)
